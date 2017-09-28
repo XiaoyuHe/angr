d3cef20121a4eef83e827c015370bace1c2eb5f0
Nilocunger <mastakata3@yahoo.com>
2017-09-27 23:58:02 -0700

    Update to use new IRSB constructor

--- angr/engines/vex/engine.py
+++ angr/engines/vex/engine.py
@@ -464,7 +464,7 @@ class SimEngineVEX(SimEngine):
         l.debug("Creating pyvex.IRSB of arch %s at %#x", arch.name, addr)
         try:
             for subphase in xrange(2):
-                irsb = pyvex.IRSB(buff, addr + thumb, arch,
+                irsb = pyvex.IRSB.liftData(buff, addr + thumb, arch,
                                   num_bytes=size,
                                   num_inst=num_inst,
                                   bytes_offset=thumb,
